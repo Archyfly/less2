ef1599bf938577411596301ad57bd3e700bca3aa
Михаил Кашинин <flyaway400@gmail.com>
2019-04-02 23:28:27 +0800

    Add fixes to less2-5

--- less25_visokos.rb
+++ less25_visokos.rb
@@ -1,13 +1,10 @@
-#сперва определяем високосный ли год
-#формируем порядок для невисокосного и високосного
-#ищем
 puts "Enter your date. Enter your day number (1-31):"
   user_day = gets.chomp.to_i
 puts "Enter your month (1-12):"
   user_month = gets.chomp.to_i
 puts "Enter your year:"
   user_year = gets.chomp.to_i
-visokos = false
+
 if user_day <= 31 && user_day > 0 && user_month <= 12 && user_month > 0 && user_year > 0
   months_days = {
     1 => 31,
@@ -24,31 +21,23 @@ if user_day <= 31 && user_day > 0 && user_month <= 12 && user_month > 0 && user_
     12 => 31
   }
   if user_year % 400 == 0 
-    visokos = true
-    elsif user_year % 100 == 0 
-    visokos = false
-    elsif user_year % 4 == 0
-    visokos = true
-  end
-  puts visokos
-
-  if visokos 
     months_days[2] = 29
-    day_numbers = Hash[months_days.zip(1..366).to_a]
-  else
+    elsif user_year % 100 == 0 
     months_days[2] = 28
-    day_numbers = Hash[months_days.zip(1..365).to_a]	
+    elsif user_year % 4 == 0
+    months_days[2] = 29
   end
 
   order_num = 0 # переменная для суммирования порядкового номера
   months_days.delete_if { |key, value| key >= user_month } # убираем из хэша лишние месяцы
 
-  if user_month > 1 
-    months_days.each_value { |value| 
-    order_num = order_num + value 
-    }
-    puts "Порядковый номер вашей даты = #{order_num + user_day}"
+  months_days.each {|key, value| 
+  if key < user_month 
+    order_num = months_days[key] + order_num
   end
+  }
+  puts "Your date serial number is: #{order_num + user_day}"
+  
 else
   puts "Enter correct date"
 end
