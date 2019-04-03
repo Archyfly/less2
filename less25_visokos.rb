fc3ce50d30a3e1b04c7a20bffabdb7fa529a5218
Archyfly <47150518+Archyfly@users.noreply.github.com>
2019-04-03 13:28:48 +0800

    Add files via upload
    Fixed with last comments

--- less25_visokos.rb
+++ less25_visokos.rb
@@ -7,37 +7,29 @@ puts "Enter your year:"
 
 if user_day <= 31 && user_day > 0 && user_month <= 12 && user_month > 0 && user_year > 0
   months_days = {
-    1 => 31,
-    2 => 28,
-    3 => 31,
-    4 => 30, 
-    5 => 31,
-    6 => 30,
-    7 => 31,
-    8 => 31,
-    9 => 30,
-    10 => 31,
-    11 => 30, 
-    12 => 31
+      1 => 31,
+      2 => 28,
+      3 => 31,
+      4 => 30,
+      5 => 31,
+      6 => 30,
+      7 => 31,
+      8 => 31,
+      9 => 30,
+      10 => 31,
+      11 => 30,
+      12 => 31
   }
-  if user_year % 400 == 0 
-    months_days[2] = 29
-    elsif user_year % 100 == 0 
-    months_days[2] = 28
-    elsif user_year % 4 == 0
+  if user_year % 400 == 0 || user_year % 4 == 0 && user_year % 100 != 0
     months_days[2] = 29
   end
-
-  order_num = 0 # переменная для суммирования порядкового номера
-  months_days.delete_if { |key, value| key >= user_month } # убираем из хэша лишние месяцы
-
-  months_days.each {|key, value| 
-  if key < user_month 
+  order_num = 0 #переменная для суммирования порядкового номера
+  months_days.each_key { |key|
+  if key < user_month
     order_num = months_days[key] + order_num
   end
   }
   puts "Your date serial number is: #{order_num + user_day}"
-  
 else
-  puts "Enter correct date"
+  puts "Enter correct date!"
 end
